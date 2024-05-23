e238e076ea92467feb23944f3de90413cf10602e
Yohan-Launay <yohandeveloppe@gmail.com>
2024-05-23 21:31:14 +0200

    correction

--- app/models/ingredient.ts
+++ app/models/ingredient.ts
@@ -1,7 +1,6 @@
 import { DateTime } from 'luxon'
-import type { ManyToMany , HasMany } from '@adonisjs/lucid/types/relations'
-import { column, BaseModel, manyToMany, hasMany } from '@adonisjs/lucid/orm'
-import Cocktail from './cocktail.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import { column, BaseModel, hasMany } from '@adonisjs/lucid/orm'
 import CocktailIngredient from './cocktail_ingredient.js'
 
 export default class Ingredient extends BaseModel {
--- database/factories/cocktail_factory.ts
+++ database/factories/cocktail_factory.ts
@@ -1,7 +1,6 @@
 import factory from '@adonisjs/lucid/factories'
 import Cocktail from '#models/cocktail'
 import { CategoryFactory } from './category_factory.js'
-import { IngredientFactory } from './ingredient_factory.js'
 import { UtensilFactory } from './utensil_factory.js'
 
 export const CocktailFactory = factory
